7c041a9c221b8523bbd8357a41fde1c4ba571971
ECT | Danielkristu <eagleies.id@gmail.com>
2026-03-27 23:26:42 +0700

    fix

--- proxy.ts
+++ proxy.ts
@@ -14,9 +14,11 @@ export default clerkMiddleware(async (auth, req) => {
   }
 });
 
+
 export const config = {
   matcher: [
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     '/(api|trpc)(.*)',
   ],
+  runtime: 'edge',
 };
